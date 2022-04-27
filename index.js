181214ce248d0342fc08faec2fc6378b35b61272
Travis Fehr <travisefehr@gmail.com>
2022-04-27 11:01:41 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function saturdayFun(hobby = 'roller-skate') {
+  return `This Saturday, I want to ${hobby}!`;
+}
+const mondayWork = function (hobby = 'go to the office') {
+  return `This Monday, I will ${hobby}.`;
+}
+function wrapAdjective(e='*') {
+  return function(adj='special') {
+    return `You are ${e}${adj}${e}!`;
+  }
+}
+wrapAdjective('a dedicated programmer', '||');
+
